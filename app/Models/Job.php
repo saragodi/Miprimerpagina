318471abd5b957ee300ed204c782022953e60302
noehassiel <hassiel-monterrosas@hotmail.com>
2023-03-26 18:14:52 -0600

    relacion@

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+
+    public function applicants()
+    {
+        return $this->hasMany(Applicant::class);
+    }
 }
