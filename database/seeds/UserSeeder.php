4e1843082e5d5fa632f44d1d72270673aba80964
noehassiel <hassiel-monterrosas@hotmail.com>
2023-05-02 21:28:38 -0600

    update users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -10,30 +10,30 @@ class UserSeeder extends Seeder
     public function run()
     {
         $user = User::create([
-        	'name' => "Webmaster",
-        	'email' => "webmaster@test.com",
-        	'password' => bcrypt('test12345'),
+            'name' => "Webmaster",
+            'email' => "webmaster@derch.mx",
+            'password' => bcrypt('derch12345'),
         ]);
         $user->assignRole('webmaster');
 
         $user = User::create([
-        	'name' => "Administrador",
-        	'email' => "admin@test.com",
-        	'password' => bcrypt('test12345'),
+            'name' => "Administrador",
+            'email' => "admin@derch.mx",
+            'password' => bcrypt('derch12345'),
         ]);
         $user->assignRole('admin');
 
         $user = User::create([
             'name' => "Analista",
-            'email' => "analista@test.com",
-            'password' => bcrypt('test12345'),
+            'email' => "analista@derch.mx",
+            'password' => bcrypt('derch12345'),
         ]);
         $user->assignRole('analyst');
 
         $user = User::create([
-        	'name' => "Cliente",
-        	'email' => "cliente@test.com",
-        	'password' => bcrypt('test12345'),
+            'name' => "Cliente",
+            'email' => "cliente@derch.mx",
+            'password' => bcrypt('derch12345'),
         ]);
         $user->assignRole('customer');
     }
